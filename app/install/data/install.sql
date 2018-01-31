4b314f9b4333eb28bf2de90db640e7ae67fac288
andy <an_hl@qq.com>
2018-01-31 10:46:54 +0800

    调整入口文件&删除安装初始文件

--- app/install/data/install.sql
+++ app/install/data/install.sql
@@ -16,46 +16,45 @@ SET time_zone = "+00:00";
 -- Table structure for cloud_sys_conf
 -- ----------------------------
 DROP TABLE IF EXISTS `cloud_sys_conf`;
+
 CREATE TABLE `cloud_sys_conf` (
-  `id` int(10) unsigned NOT NULL COMMENT '配置ID',
+  `id` int(10) UNSIGNED NOT NULL COMMENT '配置ID',
   `name` varchar(100) NOT NULL DEFAULT '' COMMENT '配置名称',
   `title` varchar(50) NOT NULL COMMENT '配置说明',
   `value` text NOT NULL COMMENT '配置值',
   `options` varchar(255) NOT NULL COMMENT '配置额外值',
   `function` varchar(60) NOT NULL COMMENT '关联函数',
-  `group` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '配置分组',
+  `group` tinyint(3) UNSIGNED NOT NULL DEFAULT '0' COMMENT '配置分组',
   `sub_group` tinyint(3) DEFAULT '0' COMMENT '子分组，子分组需要自己定义',
   `type` varchar(16) NOT NULL DEFAULT '0' COMMENT '配置类型',
   `remark` varchar(500) NOT NULL COMMENT '配置说明',
-  `create_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
-  `update_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
-  `sort` smallint(3) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `update_time` int(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '更新时间',
+  `sort` smallint(3) UNSIGNED NOT NULL DEFAULT '0' COMMENT '排序',
   `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '状态'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of cloud_sys_conf
--- ----------------------------
-INSERT INTO `cloud_sys_conf` VALUES ('1', 'toggle_web_site', '站点开关', '1', '0:关闭\r\n1:开启', '', '1', '0', 'select', '站点关闭后将提示网站已关闭，不能正常访问', '1378898976', '1406992386', '1', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('2', 'web_site_title', '网站标题', 'EacooPHP', '', '', '6', '0', 'text', '网站标题前台显示标题', '1378898976', '1507036190', '2', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('4', 'web_site_logo', '网站LOGO', '250', '', '', '6', '0', 'picture', '网站LOGO', '1407003397', '1507036190', '4', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('5', 'web_site_description', 'SEO描述', 'EacooPHP框架基于统一核心的通用互联网+信息化服务解决方案，追求简单、高效、卓越。可轻松实现支持多终端的WEB产品快速搭建、部署、上线。系统功能采用模块化、组件化、插件化等开放化低耦合设计，应用商城拥有丰富的功能模块、插件、主题，便于用户灵活扩展和二次开发。', '', '', '6', '1', 'textarea', '网站搜索引擎描述', '1378898976', '1506257875', '6', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('6', 'web_site_keyword', 'SEO关键字', '开源框架 EacooPHP ThinkPHP', '', '', '6', '1', 'textarea', '网站搜索引擎关键字', '1378898976', '1506257874', '4', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('7', 'web_site_copyright', '版权信息', 'Copyright © ******有限公司 All rights reserved.', '', '', '1', '0', 'text', '设置在网站底部显示的版权信息', '1406991855', '1468493911', '7', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('8', 'web_site_icp', '网站备案号', '豫ICP备14003306号', '', '', '6', '0', 'text', '设置在网站底部显示的备案号，如“苏ICP备1502009-2号\"', '1378900335', '1507036190', '8', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('9', 'web_site_statistics', '站点统计', '', '', '', '1', '0', 'textarea', '支持百度、Google、cnzz等所有Javascript的统计代码', '1378900335', '1415983236', '9', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('10', 'index_url', '首页地址', 'http://www.eacoo123.com', '', '', '2', '0', 'text', '可以通过配置此项自定义系统首页的地址，比如：http://www.xxx.com', '1471579753', '1506099586', '0', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('13', 'admin_tags', '后台多标签', '0', '0:关闭\r\n1:开启', '', '2', '0', 'radio', '', '1453445526', '1506099586', '3', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('14', 'admin_page_rows', '分页数量', '20', '', '', '2', '0', 'number', '分页时每页的记录数', '1434019462', '1506099586', '4', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('15', 'admin_theme', '后台主题', 'default', 'default:默认主题\r\nblue:蓝色理想\r\ngreen:绿色生活', '', '2', '0', 'select', '后台界面主题', '1436678171', '1506099586', '5', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('16', 'develop_mode', '开发模式', '1', '1:开启\r\n0:关闭', '', '3', '0', 'select', '开发模式下会显示菜单管理、配置管理、数据字典等开发者工具', '1432393583', '1507724972', '1', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('17', 'app_trace', '是否显示页面Trace', '1', '1:开启\r\n0:关闭', '', '3', '0', 'select', '是否显示页面Trace信息', '1387165685', '1507724972', '2', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('18', 'auth_key', '系统加密KEY', 'vzxI=vf[=xV)?a^XihbLKx?pYPw$;Mi^R*<mV;yJh$wy(~~E?<.JA&ANdIZ#QhPq', '', '', '3', '0', 'textarea', '轻易不要修改此项，否则容易造成用户无法登录；如要修改，务必备份原key', '1438647773', '1507724972', '3', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('19', 'only_auth_rule', '权限仅验证规则表', '1', '1:开启\n0:关闭', '', '4', '0', 'radio', '开启此项，则后台验证授权只验证规则表存在的规则', '1473437355', '1473437355', '0', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('20', 'static_domain', '静态文件独立域名', '', '', '', '3', '0', 'text', '静态文件独立域名一般用于在用户无感知的情况下平和的将网站图片自动存储到腾讯万象优图、又拍云等第三方服务。', '1438564784', '1438564784', '3', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('21', 'config_group_list', '配置分组', '1:基本\r\n2:系统\r\n3:开发\r\n4:安全\r\n5:授权\r\n6:网站信息\r\n7:用户\r\n8:邮箱', '', '', '3', '0', 'array', '配置分组的键值对不要轻易改变', '1379228036', '1510758405', '5', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('22', 'app_key', 'appid', '41', '1:开启\r\n0:关闭', '', '3', '0', 'text', '', '2018', '2018', '2', '1');
-INSERT INTO `cloud_sys_conf` VALUES ('23', 'app_secret', '系统加密KEY', '04bb466b178561fa49fccfcb18561916', '', '', '0', '0', '0', '', '0', '0', '0', '0');
+INSERT INTO `cloud_sys_conf` (`id`, `name`, `title`, `value`, `options`, `function`, `group`, `sub_group`, `type`, `remark`, `create_time`, `update_time`, `sort`, `status`) VALUES
+(1, 'toggle_web_site', '站点开关', '1', '0:关闭\r\n1:开启', '', 1, 0, 'select', '站点关闭后将提示网站已关闭，不能正常访问', 1378898976, 1406992386, 1, 1),
+(2, 'web_site_title', '网站标题', '云采集开源框架', '', '', 6, 0, 'text', '网站标题前台显示标题', 1378898976, 1507036190, 2, 1),
+(4, 'web_site_logo', '网站LOGO', '250', '', '', 6, 0, 'picture', '网站LOGO', 1407003397, 1507036190, 4, 1),
+(5, 'web_site_description', 'SEO描述', '云采集开源框架 ThinkPHP', '', '', 6, 1, 'textarea', '网站搜索引擎描述', 1378898976, 1506257875, 6, 1),
+(6, 'web_site_keyword', 'SEO关键字', 'Finndy cloud是基于ThinkPHP5开发的一套轻量级云采集框架，追求高效，简单，灵活。', '', '', 6, 1, 'textarea', '网站搜索引擎关键字', 1378898976, 1506257874, 4, 1),
+(7, 'web_site_copyright', '版权信息', 'Copyright © ******有限公司 All rights reserved.', '', '', 1, 0, 'text', '设置在网站底部显示的版权信息', 1406991855, 1468493911, 7, 1),
+(8, 'web_site_icp', '网站备案号', '豫ICP备14003306号', '', '', 6, 0, 'text', '设置在网站底部显示的备案号，如“苏ICP备1502009-2号\"', 1378900335, 1507036190, 8, 1),
+(9, 'web_site_statistics', '站点统计', '', '', '', 1, 0, 'textarea', '支持百度、Google、cnzz等所有Javascript的统计代码', 1378900335, 1415983236, 9, 1),
+(10, 'index_url', '首页地址', 'http://cloud.abc.com', '', '', 2, 0, 'text', '可以通过配置此项自定义系统首页的地址，比如：http://www.xxx.com', 1471579753, 1506099586, 0, 1),
+(13, 'admin_tags', '后台多标签', '0', '0:关闭\r\n1:开启', '', 2, 0, 'radio', '', 1453445526, 1506099586, 3, 1),
+(14, 'admin_page_rows', '分页数量', '20', '', '', 2, 0, 'number', '分页时每页的记录数', 1434019462, 1506099586, 4, 1),
+(15, 'admin_theme', '后台主题', 'default', 'default:默认主题\r\nblue:蓝色理想\r\ngreen:绿色生活', '', 2, 0, 'select', '后台界面主题', 1436678171, 1506099586, 5, 1),
+(16, 'develop_mode', '开发模式', '1', '1:开启\r\n0:关闭', '', 3, 0, 'select', '开发模式下会显示菜单管理、配置管理、数据字典等开发者工具', 1432393583, 1507724972, 1, 1),
+(17, 'app_trace', '是否显示页面Trace', '1', '1:开启\r\n0:关闭', '', 3, 0, 'select', '是否显示页面Trace信息', 1387165685, 1507724972, 2, 1),
+(18, 'auth_key', '系统加密KEY', 'vzxI=vf[=xV)?a^XihbLKx?pYPw$;Mi^R*<mV;yJh$wy(~~E?<.JA&ANdIZ#QhPq', '', '', 3, 0, 'textarea', '轻易不要修改此项，否则容易造成用户无法登录；如要修改，务必备份原key', 1438647773, 1507724972, 3, 1),
+(19, 'only_auth_rule', '权限仅验证规则表', '1', '1:开启\n0:关闭', '', 4, 0, 'radio', '开启此项，则后台验证授权只验证规则表存在的规则', 1473437355, 1473437355, 0, 1),
+(20, 'static_domain', '静态文件独立域名', '', '', '', 3, 0, 'text', '静态文件独立域名一般用于在用户无感知的情况下平和的将网站图片自动存储到腾讯万象优图、又拍云等第三方服务。', 1438564784, 1438564784, 3, 1),
+(21, 'config_group_list', '配置分组', '1:基本\r\n2:系统\r\n3:开发\r\n4:安全\r\n5:授权\r\n6:网站信息\r\n7:用户\r\n8:邮箱', '', '', 3, 0, 'array', '配置分组的键值对不要轻易改变', 1379228036, 1510758405, 5, 1),
+(22, 'app_key', 'appid', '', '1:开启\r\n0:关闭', '', 3, 0, 'text', '', 2018, 2018, 2, 1),
+(23, 'app_secret', '系统加密KEY', '', '', '', 0, 0, '0', '', 0, 0, 0, 0);
 
 -- ----------------------------
 -- Table structure for cloud_users
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// [ 应用入口文件 ]
+
+// 定义应用目录
+header("Content-type: text/html; charset=utf-8");
+if (version_compare(PHP_VERSION, '5.5', '<')) {
+    die('PHP版本过低，最少需要PHP5.5，请升级PHP版本！');
+}
+
+// 定义应用目录
+define('APP_PATH', __DIR__ . '/app/');
+
+// 定义应用目录
+define('APP_COMMON_PATH', APP_PATH . 'common/');
+
+
+// 加载框架引导文件
+require __DIR__ . '/thinkphp/start.php';
+
