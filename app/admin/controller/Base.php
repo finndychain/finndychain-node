ce894479bec94c0be6a9033a9dcd452ed877252f
andy <an_hl@qq.com>
2018-02-06 11:32:25 +0800

    Base权限修改

--- app/admin/controller/Base.php
+++ app/admin/controller/Base.php
@@ -17,10 +17,11 @@ class Base extends  Controller
             $this->error('您尚未登录系统',url('login/dologin'));
         }
 
-        if(empty($this->getSysConfValue('app_key'))&&$this->request->controller() != 'Sysconf'){
-            $this->error('未填写授权信息','Sysconf/SysSet');
+        if(empty($this->getSysConfValue('app_key'))||empty($this->getSysConfValue('app_secret'))){
+           if($this->request->controller() != 'Sysconf'){
+               $this->error('未填写授权信息','Sysconf/SysSet');
+           }
         }
-
 
     }
 
